394e716597ad796a12884b07cf574b0ea57cc2a5
emily <emily@bacheconsulting.com>
2023-10-27 13:56:07 +0200

    use decimal for money

--- csharp/ShoppingBasketDesignIdeas/ShoppingBasket1.Tests/BasketTest.cs
+++ csharp/ShoppingBasketDesignIdeas/ShoppingBasket1.Tests/BasketTest.cs
@@ -10,14 +10,14 @@ public class BasketTest
     public void Total_Over_100_Gives_Five_Percent_Discount()
     {
         var basket = new ShoppingBasket();
-        var itemA = new BasketItem("A", 10);
-        var itemB = new BasketItem("B", 25);
-        var itemC = new BasketItem("C", 9.99);
+        var itemA = new BasketItem("A", new decimal(10));
+        var itemB = new BasketItem("B", new decimal(25));
+        var itemC = new BasketItem("C", new decimal(9.99));
         basket.Add(itemA, 5);
         basket.Add(itemB, 2);
         basket.Add(itemC, 6);
         
         Assert.That(basket.GetQuantity(itemC), Is.EqualTo(6));
-        Assert.That(basket.CalculateTotal(), Is.EqualTo(151.94).Within(0.01));
+        Assert.That(basket.CalculateTotal(), Is.EqualTo(new decimal(151.94)).Within(0.01));
     }
 }
--- csharp/ShoppingBasketDesignIdeas/ShoppingBasket1/BasketItem.cs
+++ csharp/ShoppingBasketDesignIdeas/ShoppingBasket1/BasketItem.cs
@@ -2,7 +2,7 @@ namespace ShoppingBasket1;
 
 public class BasketItem
 {
-    public BasketItem(string name, double price)
+    public BasketItem(string name, decimal price)
     {
         throw new NotImplementedException();
     }
