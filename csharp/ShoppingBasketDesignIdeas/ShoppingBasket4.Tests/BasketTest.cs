b0df0ac8c33d3fee4689151e1d25dc91d8fb6c8f
emily <emily@bacheconsulting.com>
2023-10-27 13:51:38 +0200

    fourth example in C#

---
+++ csharp/ShoppingBasketDesignIdeas/ShoppingBasket4.Tests/BasketTest.cs
@@ -0,0 +1,49 @@
+namespace ShoppingBasket4.Tests;
+
+using NUnit.Framework;
+using System.Text.Json;
+
+[TestFixture]
+public class BasketTest
+{
+    [Test]
+    public void Total_Over_100_Gives_Five_Percent_Discount()
+    {
+        string json = @"
+{
+  ""items"": [
+    {
+      ""name"": ""A"",
+      ""price"": 10.0,
+    },
+    {
+      ""name"": ""B"",
+      ""price"": 25.0,
+    },
+    {
+      ""name"": ""C"",
+      ""price"": 9.99,
+    }
+  ],
+  ""quantities"": [
+    {
+      ""name"": ""A"",
+      ""quantity"": 5
+    },
+    {
+      ""name"": ""B"",
+      ""quantity"": 2
+    },
+    {
+      ""name"": ""C"",
+      ""quantity"": 6
+    }
+  ]
+}
+";
+        var basket = ShoppingBasket.FromJson(json);
+
+        Assert.That(basket.GetQuantity("C"), Is.EqualTo(6));
+        Assert.That(basket.CalculateTotal(), Is.EqualTo(151.94).Within(0.01));
+    }
+}
---
+++ csharp/ShoppingBasketDesignIdeas/ShoppingBasket4/ShoppingBasket.cs
@@ -0,0 +1,19 @@
+namespace ShoppingBasket4;
+
+public class ShoppingBasket
+{
+    public int GetQuantity(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double CalculateTotal()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static ShoppingBasket FromJson(string json)
+    {
+        throw new NotImplementedException();
+    }
+}
